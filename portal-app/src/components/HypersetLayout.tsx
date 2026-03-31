1943bcfd56bd7cff18c6803998140f98af20a748
CheezeLover <vincent@croise.eu>
2026-03-31 13:26:36 +0200

    correction

--- portal-app/src/components/HypersetLayout.tsx
+++ portal-app/src/components/HypersetLayout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect, useRef } from "react";
 import { ServiceColumn } from "./ServiceColumn";
 import { AdminModal } from "./AdminModal";
 
