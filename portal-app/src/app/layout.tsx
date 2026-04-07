57358ed182f1ef766d3b7062fc272796be0e65e8
vincent-ginini <261498700+CheezeLover@users.noreply.github.com>
2026-04-07 12:01:55 +0200

    Revert "cursor"
    This reverts commit 2bde06d4e9bb115220fff6039371f216198bcc89.

--- portal-app/src/app/layout.tsx
+++ portal-app/src/app/layout.tsx
@@ -126,8 +126,8 @@ export default async function RootLayout({
         )}
       </head>
       <body data-user-id={user.id} data-is-admin={user.isAdmin ? "true" : "false"}>
-        <CustomCursor />
         <ThemeProvider>
+          <CustomCursor />
           {children}
         </ThemeProvider>
       </body>
